c51c0344902e178d1b3b317d9ccec63d336d6bc1
renanhorste <renan.horste@gmail.com>
2021-08-06 19:37:11 -0300

    update routes

--- src/index.js
+++ src/index.js
@@ -12,16 +12,16 @@ app.use(express.json());
     console.log(owner);
     
     return response.json({message: "resposta enviada ao servidor"});
-    
-    
-});
+   });
 
  app.post('/projects', (request,response) => {
-
+   const {title, owner} = request.body;
+   console.log(title, owner);
  });
 
  app.put('/projects/:id', (request, response) => {
-
+   const {id} = request.params;
+   console.log(id);
  });
 
  app.delete('/projects/:id', (request, response) => {
