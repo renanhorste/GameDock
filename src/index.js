8b481658dc918bc869212e03ca871c5080925353
renanhorste <renan.horste@gmail.com>
2021-08-10 10:43:00 -0300

    beggining of middleware implementation

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ app.use(express.json());
 
 const games = [];
 
+function logRequest(request, response, next){
+  const {method, url} = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.log(logLabel);
+  return next();
+}
+
+  app.use(logRequest);
+
  app.get('/games', (request,response)=>{
     const {title} = request.query;
 
